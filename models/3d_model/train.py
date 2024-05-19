fix lsd error masking + filter script

models/3d_model/train.py
@@ -147,6 +147,7 @@ def train(
 
     #pipeline += SmoothArray(raw)
 
+    pipeline += gp.GrowBoundary(labels, mask=unlabelled, only_xy=True)
     pipeline += AddLocalShapeDescriptor(
             labels,
             gt_lsds,
@@ -156,7 +157,7 @@ def train(
             sigma=sigma,
             downsample=2,
     )
-    pipeline += UnmaskBackground(lsds_weights, labels_mask)
+    #pipeline += UnmaskBackground(lsds_weights, labels_mask)
     pipeline += gp.GrowBoundary(labels, mask=unlabelled, only_xy=True)
 
     pipeline += gp.AddAffinities(
@@ -169,7 +170,7 @@ def train(
         dtype=np.float32,
     )
 
-    pipeline += UnmaskBackground(gt_affs_mask, labels_mask)
+    #pipeline += UnmaskBackground(gt_affs_mask, labels_mask)
     pipeline += gp.BalanceLabels(gt_affs, affs_weights, mask=gt_affs_mask)
 
     pipeline += gp.IntensityScaleShift(raw, 2, -1)

scripts/compute_lsd_errors.py
@@ -1,21 +1,40 @@
 import time
-import yaml
-import os
-import gunpowder as gp
-from funlib.persistence import prepare_ds, open_ds
-from funlib.geometry import Coordinate, Roi
-
-from lsd.train.gp import AddLocalShapeDescriptor
-
-from scipy.ndimage import binary_dilation, binary_erosion
-from skimage.morphology import ball, disk
-
 import json
 import logging
 import sys
 import numpy as np
+import yaml
+import os
+
+from scipy.ndimage import binary_dilation, binary_erosion
+from skimage.morphology import ball, disk
+
+import gunpowder as gp
+from funlib.persistence import prepare_ds, open_ds
+from funlib.geometry import Coordinate, Roi
+from lsd.train.gp import AddLocalShapeDescriptor
+
+from lsd_error_stats import compute_stats 
+
 
 logging.basicConfig(level=logging.INFO)
+
+
+def calc_max_padding(output_size, voxel_size, sigma, mode="shrink"):
+
+    method_padding = Coordinate((sigma * 3,) * 3)
+
+    diag = np.sqrt(output_size[1] ** 2 + output_size[2] ** 2)
+
+    max_padding = Roi(
+        (
+            Coordinate([i / 2 for i in [output_size[0], diag, diag]])
+            + method_padding
+        ),
+        (0,) * 3,
+    ).snap_to_grid(voxel_size, mode=mode)
+
+    return max_padding.get_begin()
 
 
 class ComputeDistance(gp.BatchFilter):
@@ -29,28 +48,24 @@ class ComputeDistance(gp.BatchFilter):
 
     def setup(self):
 
+        spec = self.spec[self.a].copy()
         self.provides(
             self.diff,
-            self.spec[self.a].copy())
-
-    def prepare(self, request):
-        
-        deps = gp.BatchRequest()
-
-        deps[self.a] = request[self.a].copy()
-        deps[self.b] = request[self.b].copy()
-        deps[self.mask] = request[self.mask].copy()
-        
-        return deps
+            spec)
 
     def process(self, batch, request):
 
-        a_data = batch[self.a].data
-        b_data = batch[self.b].data
-        mask_data = batch[self.mask].data
+        crop_roi = request[self.diff].roi
+
+        a_data = batch[self.a].crop(crop_roi).data
+        b_data = batch[self.b].crop(crop_roi).data
+        mask_data = batch[self.mask].crop(crop_roi).data
 
         diff_data = np.sum((a_data - b_data)**2, axis=0)
         diff_data *= mask_data
+
+        spec = batch[self.a].spec.copy()
+        spec.roi = crop_roi
 
         # normalize
         epsilon = 1e-10  # a small constant to avoid division by zero
@@ -63,16 +78,18 @@ class ComputeDistance(gp.BatchFilter):
 
         batch[self.diff] = gp.Array(
             diff_data,
-            batch[self.a].spec.copy())
+            spec
+        )
 
 
 class Threshold(gp.BatchFilter):
 
-    def __init__(self, i, o, floor=0.0, ceil=1.0):
+    def __init__(self, i, o, floor=0.0, ceil=1.0, grow=(0,0,0)):
         self.i = i # input array key
         self.o = o # output array key
         self.floor = floor
         self.ceil = ceil
+        self.grow = Coordinate(grow)
 
     def setup(self):
         up_spec = self.spec[self.i].copy()
@@ -80,8 +97,16 @@ class Threshold(gp.BatchFilter):
         self.provides(self.o,up_spec)
 
     def prepare(self, request):
+
+        roi = request[self.i].roi
+
+        grow = self.grow 
+        grown_roi = roi.grow(grow,grow)
+        grown_roi = request[self.i].roi.union(grown_roi)
+
         deps = gp.BatchRequest()
         deps[self.i] = request[self.i].copy()
+        deps[self.i].roi = grown_roi
         return deps
     
     def process(self, batch, request):
@@ -94,19 +119,20 @@ class Threshold(gp.BatchFilter):
         z_struct = np.stack([ball(1)[0],]*3)
         xy_struct = np.stack([np.zeros((3,3)),disk(1),np.zeros((3,3))])
 
+        # to join gaps between z-splits in error mask
         o_data = binary_dilation(o_data, z_struct)
         o_data = binary_erosion(o_data, z_struct)
         
-        o_data = binary_dilation(o_data, xy_struct, iterations=2)
+        # to remove minor pixel-wise differences along xy boundaries
         o_data = binary_erosion(o_data, xy_struct, iterations=4)
-        o_data = binary_dilation(o_data, xy_struct, iterations=2)
+        o_data = binary_dilation(o_data, xy_struct, iterations=4)
 
         o_data = o_data.astype(np.uint8)
 
         spec = batch[self.i].spec.copy()
         spec.dtype = np.uint8
 
-        batch[self.o] = gp.Array(o_data,spec)
+        batch[self.o] = gp.Array(o_data,spec).crop(request[self.o].roi)
 
 
 def compute_lsd_errors(
@@ -118,7 +144,8 @@ def compute_lsd_errors(
         mask_dataset,
         out_file,
         out_map_dataset,
-        out_mask_dataset):
+        out_mask_dataset,
+        return_arrays=False):
 
     # array keys
     seg = gp.ArrayKey('SEGMENTATION')
@@ -137,25 +164,28 @@ def compute_lsd_errors(
     print(f"seg roi: {seg_roi}, pred_roi: {pred_roi}, mask_roi: {mask_roi}, intersection: {roi}")
 
     # io shapes
-    input_shape = [20, 212 + 420, 212 + 420]
-    output_shape = [4, 120 + 420, 120 + 420]
+    xy_increase = 0
+    input_shape = [32, 160 + xy_increase, 160 + xy_increase]
+    output_shape = [8, 100 + xy_increase, 100 + xy_increase]
+
     voxel_size = pred_ds.voxel_size
     sigma = 80
+    erode_grow = Coordinate((1,4,4)) * voxel_size
  
     voxel_size = Coordinate(voxel_size)
     input_size = Coordinate(input_shape) * voxel_size
     output_size = Coordinate(output_shape) * voxel_size
-    context = (input_size - output_size) / 2
+    context = calc_max_padding(output_size, voxel_size, sigma)
 
     total_input_roi = roi.grow(context, context) 
-    total_output_roi = roi#.grow(-context,-context)
+    total_output_roi = roi
 
     # request 
     chunk_request = gp.BatchRequest()
     chunk_request.add(seg, input_size)
-    chunk_request.add(pred, output_size)
-    chunk_request.add(mask, output_size)
-    chunk_request.add(seg_lsds, output_size)
+    chunk_request.add(pred, input_size)
+    chunk_request.add(mask, input_size)
+    chunk_request.add(seg_lsds, input_size)
     chunk_request.add(error_map, output_size)
     chunk_request.add(error_mask, output_size)
 
@@ -183,40 +213,39 @@ def compute_lsd_errors(
         gp.ZarrSource(
                 seg_file,
                 {seg: seg_dataset},
-                {seg: gp.ArraySpec(voxel_size=voxel_size, interpolatable=False, roi=seg_roi)}
+                {seg: gp.ArraySpec(interpolatable=False)}
             ),
         gp.ZarrSource(
                 lsds_file,
                 {pred: lsds_dataset},
-                {pred: gp.ArraySpec(voxel_size=voxel_size, interpolatable=True, roi=pred_roi)}
+                {pred: gp.ArraySpec(interpolatable=True)}
             ),
         gp.ZarrSource(
                 mask_file,
                 {mask: mask_dataset},
-                {mask: gp.ArraySpec(voxel_size=voxel_size, interpolatable=False, roi=mask_roi)}
+                {mask: gp.ArraySpec(interpolatable=False)}
             ),
     )
 
     pipeline = sources + gp.MergeProvider()
 
-    pipeline += gp.Pad(seg, size=context, mode="reflect")
-    pipeline += gp.Pad(pred, size=context, mode="reflect")
-    pipeline += gp.Pad(mask, size=context)
+    pipeline += gp.Pad(seg, context)
     pipeline += gp.Normalize(pred)
 
     pipeline += AddLocalShapeDescriptor(
         seg,
         seg_lsds,
         sigma=sigma,
-        downsample=2)
+        downsample=4)
     
     pipeline += ComputeDistance(
         seg_lsds,
         pred,
         error_map,
-        mask)
+        mask,
+    )
 
-    pipeline += Threshold(error_map, error_mask, floor=0.1) # 90% confidence
+    pipeline += Threshold(error_map, error_mask, floor=0.3, grow=erode_grow) # 90% confidence
 
     pipeline += gp.ZarrWrite(
             dataset_names={
@@ -225,15 +254,24 @@ def compute_lsd_errors(
             },
             store=out_file
         )
-    pipeline += gp.Scan(chunk_request, num_workers=80)
+    pipeline += gp.Scan(chunk_request, num_workers=20)
 
+    # request
     predict_request = gp.BatchRequest()
+    
+    if return_arrays:
+        predict_request[error_map] = total_output_roi
+        predict_request[error_mask] = total_output_roi
 
-    print("Starting prediction...")
-    with gp.build(pipeline):
-        pipeline.request_batch(predict_request)
-    print("Prediction finished")
+        with gp.build(pipeline):
+            batch = pipeline.request_batch(predict_request)
+        
+        return batch
 
+    else:
+        with gp.build(pipeline):
+            pipeline.request_batch(predict_request)
+        
 
 if __name__ == "__main__":
 
@@ -245,8 +283,23 @@ if __name__ == "__main__":
     config = yaml_config["processing"]["compute_lsd_errors"]
 
     start = time.time()
-    compute_lsd_errors(**config)
+    arrays = compute_lsd_errors(**config).arrays
     end = time.time()
 
     seconds = end - start
     logging.info(f'Total time to compute LSD errors: {seconds}')
+
+    arrays = {
+            'lsd_error_map': open_ds(config['out_file'],config['out_map_dataset']),
+            'lsd_error_mask': open_ds(config['out_file'],config['out_mask_dataset']),
+    }
+
+    if arrays is not None:
+        logging.info("Computing LSD Error statistics..")
+        stats = {}
+
+        for array_key in arrays:
+            arr = arrays[array_key].data
+            stats[str(array_key)] = compute_stats(arr) 
+
+        print(stats)