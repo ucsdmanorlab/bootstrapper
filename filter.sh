3109a8006e830430384906c5bc5dcffd9e67daab
yajivunev <vvenu@utexas.edu>
2024-05-24 14:17:00 -0500

    remove yq

--- filter.sh
+++ filter.sh
@@ -1,7 +1,7 @@
 yaml_file=$1 # /path/to/yaml/config
 
 # Check if compute_lsd_errors exists in config
-compute_lsd_errors=$(yq eval '.processing.compute_lsd_errors' "$yaml_file")
+compute_lsd_errors=$(python -c "import yaml; print(yaml.safe_load(open('$yaml_file', 'r'))['processing']['compute_lsd_errors'])")
 
 if [ -n "$compute_lsd_errors" ]; then
     python scripts/compute_errors.py $yaml_file
