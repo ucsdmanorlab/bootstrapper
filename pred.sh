c341dc4ff41450bd45727dbc0520d956db2d3c4e
yajivunev <vvenu@utexas.edu>
2024-09-16 18:03:08 -0700

    remove old organization and modules

--- pred.sh
+++
@@ -1,5 +0,0 @@
-yaml_file=$1 # /path/to/yaml/config
-model_name=$2 # "2d_model", "3d_model", "custom_model"
-
-# Run blockwise prediction
-python blockwise/01_predict_blockwise.py $yaml_file $model_name
--- train.sh
+++
@@ -1,7 +0,0 @@
-yaml_file=$1 # /path/to/yaml/config
-
-# Extract the setup_dir value from the YAML file
-setup_dir=$(python -c "import yaml; print(yaml.safe_load(open('$yaml_file', 'r'))['setup_dir'])")
-
-# Run the Python command in the setup_dir directory
-python $setup_dir/train.py $yaml_file
