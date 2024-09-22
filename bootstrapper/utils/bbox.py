d01145090994a8ab38d7d5f0bec4c1a9744bb591
yajivunev <vvenu@utexas.edu>
2024-09-21 19:04:15 -0700

    create tests

--- bootstrapper/utils/bbox.py
+++ bootstrapper/utils/bbox.py
@@ -4,19 +4,23 @@ from funlib.persistence import open_ds, prepare_ds
 import click
 
 @click.command()
-@click.option('--in_array', '-i', type=click.Path(exists=True), required=True, help='Path to input Zarr array.')
+@click.option('--in_array', '-i', type=click.Path(exists=True), required=True, help='Path to input Zarr array.', prompt='Enter the path to the input array')
 @click.option('--out_array', '-o', default=None, help='Path to output array')
-def bbox(in_array, out_array):
+@click.option('--padding', '-p', type=int, default=0, help='Padding to add to the bounding box.')
+def bbox(in_array, out_array, padding):
     """Crop an array to its bounding box."""
 
     in_ds = open_ds(in_array)
     arr = in_ds[in_ds.roi]
 
     slices = find_objects(arr > 0)[0]
+    slices = [slice(max(0, s.start - padding), min(s.stop + padding, arr.shape[i])) for i, s in enumerate(slices)]
+    slices = tuple(slices)
+
     new_offset = [in_ds.offset[i]+(slices[i].start * in_ds.voxel_size[i]) for i in range(3)]
 
     if out_array is None:
-        out_array = os.path.join(os.path.dirname(in_array), os.path.basename(in_array) + 'bbox')
+        out_array = os.path.join(os.path.dirname(in_array), os.path.basename(in_array) + '_bbox')
         click.echo(f"Output array not specified.")
 
     click.echo(f"Writing to {out_array}")
@@ -35,5 +39,8 @@ def bbox(in_array, out_array):
     )
     out_ds[out_ds.roi] = cropped_arr
 
-if __name__ == "__main__":
+    click.echo(f"Done!")
+    return out_array
+
+if __name__ == '__main__':
     bbox()
