54df8c010c066818580f848f797436ce6b4b91be
yajivunev <vvenu@utexas.edu>
2024-11-21 18:43:16 +0530

    fix sigma type bug in calc_max_padding

--- bootstrapper/gp/calc_max_padding.py
+++ bootstrapper/gp/calc_max_padding.py
@@ -6,11 +6,14 @@ def calc_max_padding(output_size, voxel_size, sigma, mode="shrink"):
 
     is_3d = int(output_size[0] / voxel_size[0]) != 1
 
+    if isinstance(sigma, int):
+        sigma = Coordinate([sigma] * 3)
+
     method_padding = Coordinate(
         (
-            sigma * 3 * int(is_3d),
-            sigma * 3,
-            sigma * 3,
+            sigma[0] * 3 * int(is_3d),
+            sigma[1] * 3,
+            sigma[2] * 3,
         )
     )
 
